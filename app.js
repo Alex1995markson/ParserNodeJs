de3a9d9974b53149bf1eb35cd1b27aa4dcee690f
Alex1995markson <alexmarksonit@gmail.com>
2020-05-30 22:11:19 +0300

    fix some bags

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ let $ = cheerio.load('our html page url here')
 
 var companiesList = [];
 
-// For each .item, we add all the structure of a company to the companiesList array
-// Don't try to understand what follows because we will do it differently.
 $('.list.items .item').each(function(index, element){
 	companiesList[index] = {};
 	var header = $(element).find('.header');
@@ -20,23 +18,23 @@ $('.list.items .item').each(function(index, element){
 	companiesList[index]['contact']['employee']['email'] = $(contact).find('[itemprop=email]').text();
 });
 
-console.log(companiesList); // Output the data in the terminal
-// const rp = require('request-promise');
-// const cherio = require('cheerio');
-// const Table = require('cli-table');
+console.log(companiesList); 
+const rp = require('request-promise');
+const cherio = require('cheerio');
+const Table = require('cli-table');
 
-// let users = [];
+let users = [];
 
-// const option = {
-//     url:'https://roomfi.ru/kovorkingi/',
-//     json:true
-// }
-// rp(option).then((data) => {
-//     let promises = [];
-//     let userData = [];
-//     for(let user of data.directory_items)
-// });
-/*
+const option = {
+     url:'https://roomfi.ru/kovorkingi/',
+     json:true
+}
+rp(option).then((data) => {
+    let promises = [];
+    let userData = [];
+    for(let user of data.directory_items)
+});
+	
 const http = require("http");
 http.createServer(function(request,response){
      
@@ -45,4 +43,3 @@ http.createServer(function(request,response){
 }).listen(3000, "127.0.0.1",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
 });
-*/
